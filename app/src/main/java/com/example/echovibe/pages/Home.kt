57ea784995ab45c205ea7b132fe25b5b3f3f9fab
Angela <nyabutiangella@gmail.com>
2025-04-02 00:10:52 +0300

    Created the recently played section.

--- app/src/main/java/com/example/echovibe/pages/Home.kt
+++ app/src/main/java/com/example/echovibe/pages/Home.kt
@@ -1,17 +1,29 @@
 package com.example.echovibe.pages
 
 import android.graphics.drawable.Icon
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollScope
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
@@ -24,13 +36,33 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.echovibe.R
 
 
+private val RecentlyPlayedData = listOf(
+    R.drawable.sad to R.string.sad,
+    R.drawable.happy to R.string.happy,
+    R.drawable.party to R.string.party,
+    R.drawable.adventure to R.string.adventure,
+    R.drawable.study to R.string.study,
+).map {
+    DrawableStringPair(it.first, it.second)
+}
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
 @Composable
 fun Home(){
     Column(modifier = Modifier
@@ -52,7 +84,10 @@ fun Home(){
                         modifier = Modifier
                             .padding(horizontal = 5.dp)
                             .size(40.dp)
-                            .background(MaterialTheme.colorScheme.onBackground, shape = RoundedCornerShape(8.dp)),
+                            .background(
+                                MaterialTheme.colorScheme.onBackground,
+                                shape = RoundedCornerShape(8.dp)
+                            ),
                         imageVector = Icons.Default.Person,
                         contentDescription = " ",
                         tint = MaterialTheme.colorScheme.tertiary
@@ -103,14 +138,74 @@ fun Home(){
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 8.dp, vertical = 5.dp)
-                        .border(1.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(8.dp))
+                        .border(
+                            1.dp,
+                            MaterialTheme.colorScheme.primary,
+                            shape = RoundedCornerShape(8.dp)
+                        )
                 )
             }
         }
-
-
+        Spacer(modifier = Modifier.height(12.dp))
+        RecentlyPlayed()
 
     }
+}
+@Composable
+fun RecentlyPlayed(modifier: Modifier = Modifier) {
+    Text(
+        text = "Recently Played",
+        color = MaterialTheme.colorScheme.secondary,
+        modifier = Modifier.padding(horizontal = 5.dp),
+        style = MaterialTheme.typography.titleMedium
+    )
+    Spacer(modifier = Modifier.height(12.dp))
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = Modifier
+    ){
+        items(RecentlyPlayedData){
+            item->
+            MoodBodyElement(item.drawable, item.text)
+        }
+    }
+}
+//How the mood body looks like
+@Composable
+fun MoodBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = MaterialTheme.colorScheme.onBackground)
+            .padding(5.dp)
+            .border(1.dp,
+            MaterialTheme.colorScheme.onBackground,
+            shape = RoundedCornerShape(8.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally
 
+    ) {
+        Image(
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(80.dp)
+                .padding(8.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(text),
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            ),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
 
 }
