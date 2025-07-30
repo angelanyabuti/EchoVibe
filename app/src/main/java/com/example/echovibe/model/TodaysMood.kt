d16518b8bdb6fa635b5c4359eb780922fb70a8e9
Angela <nyabutiangella@gmail.com>
2025-07-30 09:32:08 +0300

    Populating today's mood model.

--- app/src/main/java/com/example/echovibe/model/TodaysMood.kt
+++ app/src/main/java/com/example/echovibe/model/TodaysMood.kt
@@ -1,2 +1,10 @@
 package com.example.echovibe.model
 
+data class TodaysMood(
+    val id: String,
+    val song: String,
+    val artist: String,
+    val mood: String,
+    val date: String,
+    val audioUrl: String
+)
